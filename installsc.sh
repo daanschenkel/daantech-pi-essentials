1ad5c31b1c98ff59ce08be23ca798696d69f67cc
Pi <dtpiessentials@localhost>
2020-09-02 11:56:37 +0200

    update

--- installsc.sh
+++ installsc.sh
@@ -1,13 +1,9 @@
 echo Installing Raspotify
 echo --------------------------
 sudo apt install raspotify -y
+echo --------------------------
 echo Installing Shairport-sync
 echo --------------------------
 sudo apt install shairport-sync -y
-echo Installing whiptail
-echo --------------------------
-sudo apt install whiptail -y
-echo Copying Files...
-sudo cp ./menu.sh ~/musicmenu.sh
 echo ------------------------------
 echo Done installing dependencies! click ok to continue
